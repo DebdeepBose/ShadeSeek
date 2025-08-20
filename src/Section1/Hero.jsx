b126ee44a4d120a51d900c40372bbe7cf862aa3b
Debdeep <dbworkofficial@gmail.com>
2025-08-21 05:14:28 +0530

    lift

--- src/Section1/Hero.jsx
+++ src/Section1/Hero.jsx
@@ -3,7 +3,7 @@ import { FaLocationArrow } from "react-icons/fa";
 
 export const Hero = () => {
   return (
-    <section className="relative mb-4 h-screen w-full flex flex-col items-center select-none justify-center bg-black overflow-hidden">
+    <section className="relative mb-16 md:mb-4 h-screen w-full flex flex-col items-center select-none justify-center bg-black overflow-hidden">
       <div className="absolute inset-0 flex items-center justify-center ">
         <div className="absolute top-1/2 left-64 animate-pulse -translate-y-1/2 h-[300px] w-[300px] lg:h-[500px] lg:w-[500px] bg-blue-600 rounded-full blur-3xl opacity-60 mix-blend-screen"></div>
         <div className="absolute top-1/2 right-64 animate-pulse -translate-y-1/2 h-[300px] w-[300px] lg:h-[500px] lg:w-[500px] bg-pink-600 rounded-full blur-3xl opacity-60 mix-blend-screen"></div>
