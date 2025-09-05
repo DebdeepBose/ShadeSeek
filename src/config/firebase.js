503ae44ca982e47ef122258fc738ee84a2741d11
Debdeep <dbworkofficial@gmail.com>
2025-09-05 21:14:10 +0530

    Move Firebase API key to .env and update gitignore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,19 +1,16 @@
-
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
-
 const firebaseConfig = {
-apiKey: "AIzaSyDWIsdwgmSPf1RkAduTzT8kpbjh80cvvtc",
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "shadeseek-6e94a.firebaseapp.com",
   projectId: "shadeseek-6e94a",
   storageBucket: "shadeseek-6e94a.firebasestorage.app",
   messagingSenderId: "519333220915",
   appId: "1:519333220915:web:5016a73adba5457a7c8946",
-  measurementId: "G-3Y46JBPH9N"
+  measurementId: "G-3Y46JBPH9N",
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
